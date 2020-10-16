b16d6f8012cff130d6d02ff381a1d4875dbbae41
Anik <aniksarker1612@gmail.com>
2020-10-16 18:20:15 +0800

    file testing on air

--- index.js
+++ index.js
@@ -84,12 +84,32 @@ client.connect(err => {
             })
     })
 
-    // store customer order in database
+    // // store customer order in database
+    // app.post('/placeOrder', (req, res) => {
+    //     order = req.body;
+    //     customerOrdersCollection.insertOne(order)
+    //         .then(result => {
+    //             res.send(result.insertedCount > 0)
+    //         })
+    // })
+
     app.post('/placeOrder', (req, res) => {
-        order = req.body;
-        customerOrdersCollection.insertOne(order)
+        const file = req.files.file;
+        const name = req.body.name;
+        const description = req.body.description;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        const image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+        const src = image.img;
+
+        customerOrdersCollection.insertOne({ name, description, src, image })
             .then(result => {
-                res.send(result.insertedCount > 0)
+                res.send(result.insertedCount > 0);
             })
     })
 
